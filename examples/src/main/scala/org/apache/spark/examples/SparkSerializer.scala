7fadad8c2cbb5882e26028252eae839c77ed94f4
Raymond Liu <raymond.liu@intel.com>
2014-08-15 13:43:50 +0800

    more on SparkSerializer

--- examples/src/main/scala/org/apache/spark/examples/SparkSerializer.scala
+++ examples/src/main/scala/org/apache/spark/examples/SparkSerializer.scala
@@ -26,7 +26,7 @@ import org.apache.hadoop.io.NullWritable
 import org.apache.spark.rdd.RDD
 import org.apache.spark.storage.StorageLevel
 import scala.reflect.ClassTag
-import java.io.FileOutputStream
+import java.io.{File, BufferedOutputStream, OutputStream, FileOutputStream}
 
 
 class MyRegistrator4SparkSerializer extends KryoRegistrator {
@@ -39,9 +39,11 @@ class MyRegistrator4SparkSerializer extends KryoRegistrator {
 object SparkSerializer {
   def main(args: Array[String]) {
     if (args.length < 4) {
-      System.err.println("Usage : SparkWordCount <master> <caseNum> <partition number> <Item Number>")
-      System.err.println("case 0 : Java Serializer")
-      System.err.println("case 1 : Kryo Serializer")
+      System.err.println("Usage : SparkWordCount <master> <caseNum> <partition number> <Item Number> <file path>")
+      System.err.println("case 0 : Java Serializer write")
+      System.err.println("case 1 : Kryo Serializer write")
+      System.err.println("case 2 : Java Serializer read")
+      System.err.println("case 3 : Kryo Serializer read")
 
       System.exit(1)
     }
@@ -52,12 +54,24 @@ object SparkSerializer {
     val parNum = args(2).toInt
     val itemNum = args(3).toLong
 
+    var file: String = null
+    if (args.length == 5) {
+      file = args(4).toString
+    }
+
 
     caseNum match {
       case 0 =>
         conf.set("spark.serializer", "org.apache.spark.serializer.JavaSerializer")
 
       case 1 =>
+        conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+        conf.set("spark.kryo.registrator", classOf[MyRegistrator4SparkSerializer].getName)
+
+      case 2 =>
+        conf.set("spark.serializer", "org.apache.spark.serializer.JavaSerializer")
+
+      case 3 =>
         conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
         conf.set("spark.kryo.registrator", classOf[MyRegistrator4SparkSerializer].getName)
 
@@ -69,21 +83,63 @@ object SparkSerializer {
     val sc = new SparkContext(args(0), "SparkSerializer", conf)
     val s = "A String for Serialize"
 
-    val resultRDD = sc.makeRDD((1 to parNum), parNum).map{ x =>
-      //val f: FileOutputStream = new FileOutputStream("/dev/null")
-      var dataCount: Long = 0L
-      val ser = SparkEnv.get.serializer.newInstance()
+    if (caseNum == 0 || caseNum ==1) {
+      val resultRDD = sc.makeRDD((1 to parNum), parNum).map{ x =>
+        //val f: FileOutputStream = new FileOutputStream("/dev/null")
+        var dataCount: Long = 0L
+        val ser = SparkEnv.get.serializer.newInstance()
 
-      var num:Long = 0L
-      while(num < itemNum) {
-        num += 1
-        val data = ser.serialize(s)
-        dataCount += data.limit
+        var num:Long = 0L
+
+        if (file != null) {
+          val f = new File(file + x.toString)
+
+          val out: OutputStream = {
+            new BufferedOutputStream(new FileOutputStream(f), 1024 * 100)
+          }
+
+          val serOut = ser.serializeStream(out)
+
+          while(num < itemNum) {
+            num += 1
+            serOut.writeObject(s)
+          }
+
+          serOut.flush()
+          serOut.close()
+          dataCount = f.length()
+
+        } else {
+          while(num < itemNum) {
+            num += 1
+            val data = ser.serialize(s)
+            dataCount += data.limit
+          }
+        }
+        dataCount
       }
-      dataCount
+      resultRDD.collect().foreach(x => println("ser, size of data = " + x))
     }
 
-    resultRDD.collect().foreach(x => println("size of data = " + x))
+    if (caseNum == 2 || caseNum == 3) {
+      val resultRDD = sc.makeRDD((1 to parNum), parNum).map{ x =>
+      //val f: FileOutputStream = new FileOutputStream("/dev/null")
+        var dataCount: Long = 0L
+        val ser = SparkEnv.get.serializer.newInstance()
+
+        var num:Long = 0L
+        val data = ser.serialize(s)
+
+        while(num < itemNum) {
+          num += 1
+          ser.deserialize(data)
+          dataCount += data.limit
+        }
+
+        dataCount
+      }
+      resultRDD.collect().foreach(x => println("deser, size of data= " + x))
+    }
 
     sc.stop()
   }
